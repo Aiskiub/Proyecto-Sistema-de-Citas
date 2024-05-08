f8ae393f82198ad360c4736ba635b54295933490
Chocorramin <danilq139@gmail.com>
2024-05-07 19:22:36 -0500

    Refactor main function to include project description in print statement

--- Main.py
+++ Main.py
@@ -1,5 +1,5 @@
 def main():
-    print("Hola mi gente, aqui va ir el poderoso main 😸")
+    print("Hola mi gente, aqui va ir el poderoso main 😸 del proyecto")
 
 if __name__ == "__main__":
     main()
