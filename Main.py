6625859a401e051edbe571e99abd073d6d99e0e2
Chocorramin <danilq139@gmail.com>
2024-05-07 23:22:24 -0500

    prueba de error

--- Main.py
+++ Main.py
@@ -1,5 +1,7 @@
 def main():
-    print("Hola mi gente, aqui va ir el poderoso main 😸")
+    print("Hola mi gente, aqui va ir el poderoso main <3")
 
 if __name__ == "__main__":
     main()
+    
+    mi cambio maluco
