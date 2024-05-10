8f6a99d5be3d23e378ae672ab9cbc98bca6237b6
Rolovedo <acevedo314848@gmail.com>
2024-05-09 20:40:47 -0500

    puse soy samuel

--- Main.py
+++ Main.py
@@ -1,6 +1,6 @@
 import citas.cita as citas
 def main():
-    print("Hola mi gente, aqui va ir el poderoso main <3")
+    print("Hola mi gente, aqui va ir el poderoso main <3 soy samuel")
     print("Aqui va la cita de la semana:")
     citas.cita()
 
