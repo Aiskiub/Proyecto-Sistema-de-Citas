749f746f3dbc840c69718d6182bf46d52294de5c
Rolovedo <acevedo314848@gmail.com>
2024-05-20 18:50:19 -0500

    Clase medico subida (Necesita asignar una funcion malla_citas que evalua la disponibilidad)

--- medicos/medico.py
+++ medicos/medico.py
@@ -1,17 +1,34 @@
 import datetime
 
-def TurnosMedicos(nombre):
-    hora_actual = datetime.datetime.now()
-    entrada = hora_actual.strftime("%H:%M:%S")
-    print("Su hora de entrada es: ",entrada)
-    hora_sumada = hora_actual + datetime.timedelta(hours=10)
-    salida = hora_sumada.strftime("%H:%M:%S")
-    print("Doctor",nombre,", el consultorio 100 estará disponible para usted de",entrada,"a",salida)
+class Medico:
+    def __init__(self, nombre, apellido, registro_medico):
+        self.nombre = nombre
+        self.apellido = apellido
+        self.registro_medico = registro_medico
+        self.malla_citas = []
 
-nombre = input("Ingrese su nombre: ")
-TurnosMedicos(nombre)
+    def turnosMedicos(nombre):
+        hora_actual = datetime.datetime.now()
+        entrada = hora_actual.strftime("%H:%M:%S")
+        print("Su hora de entrada es: ",entrada)
+        hora_sumada = hora_actual + datetime.timedelta(hours=10)
+        salida = hora_sumada.strftime("%H:%M:%S")
+        print("Doctor",nombre,", el consultorio 100 estará disponible para usted de",entrada,"a",salida)
 
+    #nombre = input("Ingrese su nombre: ")
+    #TurnosMedicos(nombre)
 
+    def agregarCita(self, cita):
+        self.malla_citas.append(cita)
 
+    def cancelarCita(self, cita):
+        if cita in self.malla_citas:
+            self.malla_citas.remove(cita)
+            return True
+        return False
 
-
+    def estaDisponible(self, fecha, hora):
+        for cita in self.malla_citas:
+            if cita.fecha == fecha and cita.hora == hora:
+                return False
+        return True
