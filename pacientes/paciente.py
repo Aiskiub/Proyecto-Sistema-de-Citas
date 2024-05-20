ec736e97186c34dc8edcf790d9eebd098de07052
Chocorramin <danilq139@gmail.com>
2024-05-20 17:33:02 -0500

    chore: Add Paciente class and CRUD operations

--- pacientes/paciente.py
+++ pacientes/paciente.py
@@ -29,44 +29,47 @@ def clasificar(edad):
         
 # Definimos la clase Paciente
 class Paciente:
-    def __init__(self, nombre, apellido, tipo_documento, documento_identidad, fecha_nacimiento, edad=None, historial=None, cita=None, clasificacion=None):
+    def __init__(self, nombre, apellido, tipo_id, id, fecha_nacimiento, edad=None, historial=None, cita=None, clasificacion=None):
         self.nombre = nombre
         self.apellido = apellido
-        self.tipo_documento = tipo_documento
-        self.documento_identidad = documento_identidad
-        self.fecha_nacimiento = fecha_nacimiento
-        self.edad = calcular_edad(fecha_nacimiento)
-        self.clasificacion = clasificar(edad)
-
-
-# Definimos el CRUD   
-class GestionDePacientes:
-    def __init__(self):
-        self.pila_pacientes = []
-
-    def agregar_paciente(self, paciente):
-        self.pila_pacientes.append(paciente)
+        self.tipo_id = tipo_id
+        self.id = id
+        self.fecha_nacimiento = date.fromisoformat(fecha_nacimiento)
+        self.edad = calcular_edad(self.fecha_nacimiento)
         
-    def leer_paciente(self, documento_identidad):
-        for paciente in self.pila_pacientes:
-            if paciente.documento_identidad == documento_identidad:
-                return paciente
-        return None
+        self.clasificacion = clasificar(self.edad)
     
-    def actualizar_paciente(self, documento_identidad, paciente_actualizado):
-        for i, paciente in enumerate(self.pila_pacientes):
-            if paciente.documento_identidad == documento_identidad:
-                self.pila_pacientes[i] = paciente_actualizado
-                return True
-        return False
+    #Getters
+    def getNombre(self):
+        return self.nombre
+    def getApellido(self):
+        return self.apellido
+    def getTipoID(self):
+        return self.tipo_id
+    def getID(self):
+        return self.id
+    def getFecha(self):
+        return self.fecha_nacimiento
+    def getEdad(self):
+        return self.edad
+    def getHistorial(self):
+        return self.historial
+    def getCita(self):
+        return self.cita
+    def getClasificacion(self):
+        return self.clasificacion
     
-    def eliminar_paciente(self, documento_identidad):
-        for i, paciente in enumerate(self.pila_pacientes):
-            if paciente.documento_identidad == documento_identidad:
-                self.pila_pacientes.pop(i)
-                return True
-        return False
-    
-
-
+    #Setters
+    def setNombre(self, nombre):
+        self.nombre = nombre
+    def setApellido(self, apellido):
+        self.apellido = apellido
+    def setTipoID(self, tipo_id):
+        self.tipo_id = tipo_id
+    def setID(self, id):
+        self.id = id
+    def setFecha(self, fecha): #Si se cambia la fecha de nacimiento, automáticamente se actualizan los campos de edad y clasificación
+        self.fecha_nacimiento = date.fromisoformat(fecha)
+        self.edad = calcular_edad(self.fecha_nacimiento)
+        self.clasificacion = clasificar(self.edad)
 
