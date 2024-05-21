2ba5407868babcc451785a2d8de50563b31f2e62
Rolovedo <acevedo314848@gmail.com>
2024-05-20 19:19:21 -0500

    Clase Medico

--- medicos/medico.py
+++ medicos/medico.py
@@ -1,22 +1,9 @@
-import datetime
-
 class Medico:
     def __init__(self, nombre, apellido, registro_medico):
         self.nombre = nombre
         self.apellido = apellido
         self.registro_medico = registro_medico
         self.malla_citas = []
-
-    def turnosMedicos(nombre):
-        hora_actual = datetime.datetime.now()
-        entrada = hora_actual.strftime("%H:%M:%S")
-        print("Su hora de entrada es: ",entrada)
-        hora_sumada = hora_actual + datetime.timedelta(hours=10)
-        salida = hora_sumada.strftime("%H:%M:%S")
-        print("Doctor",nombre,", el consultorio 100 estará disponible para usted de",entrada,"a",salida)
-
-    #nombre = input("Ingrese su nombre: ")
-    #TurnosMedicos(nombre)
 
     def agregarCita(self, cita):
         self.malla_citas.append(cita)
