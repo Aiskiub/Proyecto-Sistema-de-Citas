bfa4b7dafbfac1467cd9f940a339be9571a18f7b
Rolovedo <acevedo314848@gmail.com>
2024-05-20 19:03:02 -0500

    Agregue la clase mallaCitas que evalua la disponibilidad de las citas

--- citas/cita.py
+++ citas/cita.py
@@ -1,4 +1,25 @@
 import utils.algoritmos as utils
+from collections import deque
+
+class MallaCitas:
+    def __init__(self):
+        self.citas = deque()
+
+    def agregarCita(self, cita):
+        self.citas.append(cita)
+
+    def cancelarCita(self, cita):
+        if cita in self.citas:
+            self.citas.remove(cita)
+            return True
+        return False
+
+    def buscarCita(self, fecha, hora):
+        for cita in self.citas:
+            if cita.fecha_programacion == fecha and cita.hora_asignacion == hora:
+                return cita
+        return None
+
 
 def cita():
     print('Cita de la semana:')  
