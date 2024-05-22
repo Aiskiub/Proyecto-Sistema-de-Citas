342f4e5d5d1127f8d94ac7fe678819894a0a3a52
NataliaOsorioL <126190992+NataliaOsorioL@users.noreply.github.com>
2024-05-22 12:09:35 -0500

    Update cita.py

--- citas/cita.py
+++ citas/cita.py
@@ -9,6 +9,15 @@ class Cita:
         self.hora_asignacion = hora_asignacion
         self.consultorio = consultorio
         self.estado = 'no disponible'
+
+    def asignar_medico(self, medico):
+        self.medico = medico
     
     def __str__(self):
         return f"Cita: {self.paciente.nombre} {self.paciente.apellido} con Dr. {self.medico.nombre} en consultorio {self.consultorio} el {self.fecha_programacion} a las {self.hora_asignacion}. Estado: {self.estado}"
+    
+    def cancelar(self):
+        self.estado = "disponible"
+    
+    def __repr__(self):
+        return f"Cita({self.fecha_prog.strftime('%Y-%m-%d')}, {self.hora}, {self.num_consultorio}, {self.medico.nombre}, {self.paciente.nombres}, {self.estado})"
