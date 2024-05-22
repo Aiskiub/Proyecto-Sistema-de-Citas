a59245477a16e8c75848f5bc195156ac040c3130
NataliaOsorioL <126190992+NataliaOsorioL@users.noreply.github.com>
2024-05-22 12:27:06 -0500

    Update GestionDeCitas.py

--- citas/GestionDeCitas.py
+++ citas/GestionDeCitas.py
@@ -2,7 +2,6 @@ from datetime import timedelta  # No se porque sirve con , datetime
 from datetime import datetime
 from citas.cita import Cita
 
-
 class GestionDeCitas:
     def __init__(self):
         self.medicos = []  # Lista de médicos preestablecidos
@@ -12,38 +11,41 @@ class GestionDeCitas:
         self.medicos.append(medico)
 
     def asignar_cita(self, paciente, medico_nombre, hora_inicio, duracion, consultorio):
-        hora_inicio = datetime.strptime(hora_inicio, '%Y-%m-%d %H:%M')
-        duracion = timedelta(minutes=duracion)
-        nueva_cita = Cita(paciente, None, hora_inicio, duracion, consultorio)
-
-    # Buscar al médico por nombre
+        try:
+            hora_inicio = datetime.strptime(hora_inicio, '%Y-%m-%d %H:%M')
+            duracion = timedelta(minutes=duracion)
+        except ValueError as e:
+            print(f"Error al parsear la fecha/hora: {e}")
+            return False
+        
+        nueva_cita = Cita(paciente, None, datetime.now().strftime('%Y-%m-%d'), hora_inicio.strftime('%Y-%m-%d'), hora_inicio.strftime('%H:%M'), consultorio)
+        
         for medico in self.medicos:
             if medico.nombre == medico_nombre:
-                # Verificar la disponibilidad del médico con los argumentos necesarios
-                if medico.verificar_disponibilidad(nueva_cita.fecha_programacion, nueva_cita.hora_asignacion, nueva_cita.duracion):
-                    # Si el médico está disponible, agregar la cita
+                if medico.verificar_disponibilidad(hora_inicio, nueva_cita.hora_asignacion, duracion):
+                    nueva_cita.asignar_medico(medico)
                     medico.agregar_cita(nueva_cita)
                     paciente.cita = nueva_cita
                     self.citas.append(nueva_cita)
                     print(f"Cita asignada: {nueva_cita}")
                     return True
                 else:
-                    print(
-                        f"El médico {medico.nombre} no está disponible en ese horario.")
+                    print(f"El médico {medico.nombre} no está disponible en ese horario.")
                     return False
+        
         print(f"No se encontró al médico {medico_nombre}.")
         return False
 
     def cancelar_cita(self, paciente):
         if paciente.cita:
             cita = paciente.cita
+            cita.cancelar()
             cita.medico.citas.remove(cita)
             self.citas.remove(cita)
             paciente.cita = None
             print(f"Cita cancelada para {paciente.nombre} {paciente.apellido}")
             return True
-        print(
-            f"No hay cita para cancelar para {paciente.nombre} {paciente.apellido}")
+        print(f"No hay cita para cancelar para {paciente.nombre} {paciente.apellido}")
         return False
 
     def mostrar_citas(self):
