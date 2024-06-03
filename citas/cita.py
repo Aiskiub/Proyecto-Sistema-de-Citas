9186a92d9cd22178d46534c89c98b000d2c42dd8
Chocorramin <danilq139@gmail.com>
2024-06-02 20:39:56 -0500

    añado el atributo duracion

--- citas/cita.py
+++ citas/cita.py
@@ -8,6 +8,7 @@ class Cita:
         self.hora_asignacion = hora_asignacion
         self.consultorio = medico.consultorio
         self.fechaSolicitud = datetime.now()
+        self.duracion = 30
     
     def __str__(self):
         return f"Cita: {self.paciente.nombre} {self.paciente.apellido} con Dr. {self.medico.nombre} {self.medico.apellido} en consultorio {self.medico.consultorio} el {self.fecha_programacion} a las {self.hora_asignacion}."
